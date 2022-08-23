c817ae8cdd31e17006f8e955c4c14017885cdd11
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2022-08-23 22:12:17 +0900

    MajorMainActivity 생성
    레이아웃까지만 생성
    우선 commit

--- millennium-baby/app/src/main/java/com/example/millennium_baby/MajorMainActivity.kt
+++ millennium-baby/app/src/main/java/com/example/millennium_baby/MajorMainActivity.kt
@@ -3,10 +3,9 @@ package com.example.millennium_baby
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class MajorMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_major_main)
     }
 }
