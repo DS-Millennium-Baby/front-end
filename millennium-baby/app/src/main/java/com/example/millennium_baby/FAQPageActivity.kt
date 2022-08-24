95b245f8f3bef2f55173b3ea24ac9a6e70b9a322
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2022-08-24 18:16:31 +0900

    FAQ 화면 UI, 액션 처리
    배치 수정, 그림자 효과 추가 필요

--- millennium-baby/app/src/main/java/com/example/millennium_baby/FAQPageActivity.kt
+++ millennium-baby/app/src/main/java/com/example/millennium_baby/FAQPageActivity.kt
@@ -2,10 +2,45 @@ package com.example.millennium_baby
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.RenderProcessGoneDetail
+import com.example.millennium_baby.databinding.ActivityFaqpageBinding
 
 class FAQPageActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityFaqpageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_faqpage)
+        binding = ActivityFaqpageBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.faq1Visiblebtn.setOnClickListener{
+            binding.faq1Visiblebtn.visibility  = View.GONE
+            binding.faq1Invisiblebtn.visibility = View.VISIBLE
+        }
+        binding.faq1Invisiblebtn.setOnClickListener{
+            binding.faq1Visiblebtn.visibility  = View.VISIBLE
+            binding.faq1Invisiblebtn.visibility = View.GONE
+        }
+
+        binding.faq2Visiblebtn.setOnClickListener{
+            binding.faq2Visiblebtn.visibility  = View.GONE
+            binding.faq2Invisiblebtn.visibility = View.VISIBLE
+        }
+        binding.faq2Invisiblebtn.setOnClickListener{
+            binding.faq2Visiblebtn.visibility  = View.VISIBLE
+            binding.faq2Invisiblebtn.visibility = View.GONE
+        }
+
+        binding.faq3Visiblebtn.setOnClickListener{
+            binding.faq3Visiblebtn.visibility  = View.GONE
+            binding.faq3Invisiblebtn.visibility = View.VISIBLE
+        }
+        binding.faq3Invisiblebtn.setOnClickListener{
+            binding.faq3Visiblebtn.visibility  = View.VISIBLE
+            binding.faq3Invisiblebtn.visibility = View.GONE
+        }
     }
 }
