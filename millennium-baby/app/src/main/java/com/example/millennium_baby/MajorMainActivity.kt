83204cab05a58c546981433c5d4ee2e33795aae8
Jin573 <rlawlstjs5573@duksung.ac.kr>
2022-08-25 00:00:09 +0900

    메인, 전공 메인, FAQ 화면 제작 (#9)
    * 색깔 지정 추가
    
    기본 흰색(realwhite)을 추가
    
    * MajorMainActivity 레이아웃 배치
    
    ViewPager2 동작을 위한 build.gradle 수정
    RelativeLayout, ScrollView, ViewPager2 배치
    ViewPager2 스와이프 동작 구현
    
    * ViewPager 내부 RecyclerView 생성
    
    RecyclerView 생성
    Adapter 생성
    
    * fragment 색 수정
    
    * ViewPager 레이아웃 수정
    
    * 메인 화면 생성
    
    MainPageActivity 생성
    
    * 메인 화면 UI 배치
    
    메인 화면 구성 및 아이콘 생성
    
    * FAQ 페이지 생성
    
    * FAQ 화면 UI, 액션 처리
    
    배치 수정, 그림자 효과 추가 필요
    
    * 메인 화면 UI
    
    * MainPage와 FAQPage 연결
    
    Main 페이지에서 버튼 누르면 FAQ 페이지로 연결
    
    * faq 페이지 및 main 페이지 텍스트 수정
    
    * misc파일 커밋
    
    * 전공 스크롤 화면 수정
    
    * 전공 메인화면 레이아웃 정리
    
    * 메인 레이아웃 수정
    
    Co-authored-by: SOOYEON YOO <dbtndus0527@gmail.com>

--- millennium-baby/app/src/main/java/com/example/millennium_baby/MajorMainActivity.kt
+++ millennium-baby/app/src/main/java/com/example/millennium_baby/MajorMainActivity.kt
@@ -2,10 +2,39 @@ package com.example.millennium_baby
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.millennium_baby.databinding.ActivityMajorMainBinding
+import java.util.*
 
-class MajorMainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_major_main)
+class ViewPagerFragmentAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity){
+    val FragmentList = listOf<Fragment>(QnAFragment(), TipFragment())
+
+    override fun getItemCount(): Int {
+        return FragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return FragmentList[position]
     }
 }
+
+class MajorMainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMajorMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewPagerFragmentAdapter = ViewPagerFragmentAdapter(this)
+
+        binding = ActivityMajorMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.majorHeadQna.adapter = viewPagerFragmentAdapter
+
+    }
+
+}
+
