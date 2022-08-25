a4a3f7328f1cd9e04268683d967a23f52a10f172
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2022-08-26 01:32:14 +0900

    팁 메인, 상세, 작성 페이지 생성

---
+++ millennium-baby/app/src/main/java/com/example/millennium_baby/WritingTipActivity.kt
@@ -0,0 +1,11 @@
+package com.example.millennium_baby
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WritingTipActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_writing_tip)
+    }
+}
